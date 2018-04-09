838a314142c6fca99dbf440e4d6433f581f17baf
AFudro <headcrab96@gmail.com>
2018-04-09 14:26:40 +0200

    backend init

---
+++ Backend/studycave-rest-service/src/main/java/studycave/application/StudycaveRestServiceApplication.java
@@ -0,0 +1,12 @@
+package studycave.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudycaveRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudycaveRestServiceApplication.class, args);
+	}
+}
