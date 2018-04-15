30a275680ff66751d6632e05ebb94b49aa0affdc
AFudro <andrzej.fudro@gmail.com>
2018-04-15 16:49:14 +0200

    database fix

--- Backend/studycave-db.sql
+++ Backend/studycave-db.sql
@@ -1,11 +1,10 @@
-#create database db;
-use  db;
+drop database db;
+create database db;
+use db;
 
-drop table Flashcards;
-drop table FlashcardsSet;
-drop table Users;
 
-create table Users (
+
+create table User (
 	id int,
     name varchar(15),
     surname varchar(15),
@@ -13,41 +12,40 @@ create table Users (
     password varchar(15),
     email varchar(20),
     
-
 	primary key(id)
 );
 
-create table FlashcardsSet (
+create table FlashcardSet (
 	id int,
     name varchar(30),
     category varchar(30),
     id_owner int,
     add_date date,
-    edit_add date,
+    edit_date date,
     grade int,
     
-    primary key(id),
-    foreign key(id_owner) references Users(id)
+    primary key(id)
+    #foreign key(id_owner) references User(id)
 );
 
-create table Flashcards (
+
+create table Flashcard (
 	id_set int,
-    id_card int,
+    id int,
     left_side varchar(30),
 	right_side varchar(30),
 
-	primary key(id_card),
-    foreign key(id_set) references FlashcardsSet(id)
+	primary key(id),
+    foreign key(id_set) references FlashcardSet(id)
 );
 
+INSERT INTO User VAflashcard_setLUES(0,'-','-','anonim','-','-');
+INSERT INTO FlashcardSet VALUES(0,'zestaw1','cat1',0, 14/04/2018,14/04/2018 ,5);
+INSERT INTO FlashcardSet VALUES(1,'zestaw2','cat1',0, 14/04/2018,14/04/2018 ,5);
+INSERT INTO Flashcard VALUES(0,1,'left1','right1');
+INSERT INTO Flashcard VALUES(0,2,'left2','right2');
+INSERT INTO Flashcard VALUES(1,3,'2left2','2right2');
 
-
-
-INSERT INTO Users VALUES(0,'-','-','anonim','-','-');
-INSERT INTO FlashcardsSet VALUES(0,'zestaw1','cat1',0, 14/04/2018,14/04/2018 ,5);
-INSERT INTO FlashcardsSet VALUES(2,'zestaw2','cat1',0, 14/04/2018,14/04/2018 ,5);
-INSERT INTO Flashcards VALUES(0,1,'left1','right1');
-INSERT INTO Flashcards VALUES(0,2,'left2','right2');
-INSERT INTO Flashcards VALUES(2,3,'2left2','2right2');
-
-select * from Flashcards where id_set=0
+select * from User;
+select * from FlashcardSet;
+select * from flashcard;
