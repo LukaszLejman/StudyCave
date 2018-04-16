0551c4a990d29827837de55822630b3042ea2695
Drizzet <dawid.bogacki@gmail.com>
2018-04-16 15:05:27 +0200

    csv import

--- Backend/studycave-rest-service/src/main/java/studycave/application/StudycaveRestServiceApplication.java
+++ Backend/studycave-rest-service/src/main/java/studycave/application/StudycaveRestServiceApplication.java
@@ -1,12 +1,26 @@
 package studycave.application;
 
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudycaveRestServiceApplication {
+public class StudycaveRestServiceApplication implements CommandLineRunner {
+	
+	 
+	@Resource
+	StorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudycaveRestServiceApplication.class, args);
 	}
+
+	@Override
+	public void run(String... arg) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
+	}
+	
 }
