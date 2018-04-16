b68f26699f26991d19abb11310bc220a9c58d4f6
AFudro <andrzej.fudro@gmail.com>
2018-04-16 11:01:52 +0200

    CORS fix

--- Backend/studycave-rest-service/src/main/java/studycave/application/SetController.java
+++ Backend/studycave-rest-service/src/main/java/studycave/application/SetController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.annotations.Api;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/sets")
 @Api
 public class SetController {
