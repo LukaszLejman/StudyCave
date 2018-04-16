032e7f342850a7df80b58b8702cb0066707f9146
Drizzet <dawid.bogacki@gmail.com>
2018-04-16 16:13:40 +0200

    cros fix

--- Backend/studycave-rest-service/src/main/java/studycave/application/UploadController.java
+++ Backend/studycave-rest-service/src/main/java/studycave/application/UploadController.java
@@ -16,10 +16,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
- 
+import org.springframework.web.bind.annotation.CrossOrigin; 
+
 @Controller
+@CrossOrigin
 public class UploadController {
  
 	@Autowired
