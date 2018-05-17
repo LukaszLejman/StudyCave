2be2159321ab54c685412d8d2bec33746274e405
Drizzet <dawid.bogacki@gmail.com>
2018-05-17 19:41:11 +0200

    cors -fix

--- Backend/studycave-rest-service/src/main/java/studycave/application/test/TestController.java
+++ Backend/studycave-rest-service/src/main/java/studycave/application/test/TestController.java
@@ -3,6 +3,7 @@ package studycave.application.test;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import io.swagger.annotations.Api;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("/tests")
 @Api
 public class TestController {
