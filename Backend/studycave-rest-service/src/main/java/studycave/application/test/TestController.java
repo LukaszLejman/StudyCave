4a3eabe43d45f3d968f88a784e7d1201f7e4bc66
Drizzet <dawid.bogacki@gmail.com>
2018-05-23 19:14:25 +0200

    delete test

--- Backend/studycave-rest-service/src/main/java/studycave/application/test/TestController.java
+++ Backend/studycave-rest-service/src/main/java/studycave/application/test/TestController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,11 @@ public class TestController {
 	@GetMapping("/{id}")
 	public Optional<Test> getTest(@PathVariable(required = true) Long id) {
 		return  testRepository.findById(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteTest(@PathVariable(required = true)Long id) {
+		testRepository.deleteById(id);
 	}
 	
 	@PostMapping
