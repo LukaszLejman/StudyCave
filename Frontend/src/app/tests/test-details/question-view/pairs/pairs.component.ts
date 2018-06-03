e02f3d671587be618a64e6878d281c0ec7bfc3aa
LukaszLejman <LukaszLejman96@wp.pl>
2018-06-04 00:19:28 +0200

    console log pairs body

--- Frontend/src/app/tests/test-details/question-view/pairs/pairs.component.ts
+++ Frontend/src/app/tests/test-details/question-view/pairs/pairs.component.ts
@@ -35,6 +35,7 @@ export class PairsComponent implements OnInit, OnChanges, OnDestroy {
       id: this.question.id, type: 'pairs',
       answers: answers
     };
+    console.log(body);
     this.verifyAnswerSubscription = this.testsService.verifyAnswer(this.id, body).subscribe(d => {
       $('.answers').find('[type="text"]').prop('value', '');
       this.emitNextQuestionRequest.emit(d);
