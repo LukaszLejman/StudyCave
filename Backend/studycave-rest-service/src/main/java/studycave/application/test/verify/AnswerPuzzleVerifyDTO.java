c43b980e2e01689f868101b69a936dbe3c3e6d71
AFudro <andrzej.fudro@gmail.com>
2018-06-03 16:39:07 +0200

    test verify puzzle

--- Backend/studycave-rest-service/src/main/java/studycave/application/test/verify/AnswerPuzzleVerifyDTO.java
+++ Backend/studycave-rest-service/src/main/java/studycave/application/test/verify/AnswerPuzzleVerifyDTO.java
@@ -1,5 +1,22 @@
 package studycave.application.test.verify;
 
-public class AnswerPuzzleVerifyDTO extends AnswerVerifyDTO {
+import java.util.List;
 
+public class AnswerPuzzleVerifyDTO extends AnswerVerifyDTO {
+	protected Long id;
+	protected List<String> puzzles;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public List<String> getPuzzles() {
+		return puzzles;
+	}
+	public void setPuzzles(List<String> puzzles) {
+		this.puzzles = puzzles;
+	}
+	
+	
 }
