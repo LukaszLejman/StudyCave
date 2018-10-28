69fd5cbc43e854d39e287b205d9ffa265517463a
Andrzej Fudro <andrzej.fudro@gmail.com>
2018-10-28 13:44:56 +0100

    Users builder password fix

--- Backend/studycave-rest-service/src/main/java/studycave/application/builders/UsersBuilder.java
+++ Backend/studycave-rest-service/src/main/java/studycave/application/builders/UsersBuilder.java
@@ -1,6 +1,7 @@
 package studycave.application.builders;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import studycave.application.user.User;
@@ -9,8 +10,11 @@ import studycave.application.user.UserRepository;
 @Component
 public class UsersBuilder {
 
+	@Autowired 
+	BCryptPasswordEncoder bCryptPasswordEncoder;
     private UserRepository userRepository;
-
+    
+    
     @Autowired
     public UsersBuilder(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -23,7 +27,7 @@ public class UsersBuilder {
     		User user = new User();
     		user.setEmail(String.format("u%d@email.com", i));
     		user.setName(String.format("Name%d", i));
-    		user.setPassword("123qwe");
+    		user.setPassword(bCryptPasswordEncoder.encode("123qwe"));
     		user.setSurname(String.format("Surname%d", i));
     		user.setUsername(String.format("u%d", i));
     		userRepository.save(user);    		
