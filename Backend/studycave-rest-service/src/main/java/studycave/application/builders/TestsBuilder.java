4fd97308ca4dc4907cf9062ce4651a81ac559a9a
Drizzet <dawid.bogacki@gmail.com>
2018-11-11 19:02:50 +0100

    Update Backend/studycave-rest-service/src/main/java/studycave/application/builders/TestsBuilder.java

--- Backend/studycave-rest-service/src/main/java/studycave/application/builders/TestsBuilder.java
+++ Backend/studycave-rest-service/src/main/java/studycave/application/builders/TestsBuilder.java
@@ -1,5 +1,7 @@
 package studycave.application.builders;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,38 +22,45 @@ public class TestsBuilder {
 	
 	@Autowired
 	private TestRepository testRepository;
-	private QuestionRepository questionRepository;
-	private AnswerRepository answerRepository;
+
 	
 	@Autowired TestsBuilder(TestRepository testRepository, QuestionRepository questionRepository, AnswerRepository answerRepository){
 		this.testRepository = testRepository;
-		this.questionRepository = questionRepository;
-		this.answerRepository = answerRepository;
 	}
 	
 	public void build( ) {
-		int testsNumber = 10;
+		int testsNumber = 5;
 		int questionsNumber = 5;
+		int answersNumber = 3;
 		
 		for (int i = 0; i < testsNumber; i++) {
 			Test test = new Test();
+			test.setAddDate();
+			test.setEditDate();
+			test.setIdOwner((long) 1);
+			test.setPermission("public");
+			List<Question> questions = new ArrayList<Question>();
+			Question question;
 			for (int j = 0; j < questionsNumber; j++) {
+
 				Random rand = new Random();
-//				switch(rand.nextInt(4)+1){
-//					case 1: 
-//						Question question = new QuestionChoices();
-//					case 2:
-//						Question question = new QuestionGaps();
-//					case 3:
-//						Question question = new QuestionPairs();
-//					case 4:
-//						Question question = new QuestionPuzzle();
-//				}
-				
-					
+				switch(rand.nextInt(4)+1){
+					case 1: 
+						question = new QuestionChoices();
+					case 2:
+						question = new QuestionGaps();
+					case 3:
+						question = new QuestionPairs();
+					case 4:
+						question = new QuestionPuzzle();
+				}
+//				question.setNrQuestion(j);
+//				question.setText("question ???");
+//			questions.add(question);		
 				
 			}
-			//test.setQuestions(questions);
+			test.setQuestions(questions);
+			testRepository.save(test);
 		}
 		System.out.print("Tests inserted");
 	}
