50db5cc8d0ab64737ab3ae5ed1e718361e393abd
LukaszLejman <LukaszLejman96@wp.pl>
2018-11-18 12:51:02 +0100

    [#161931882] join to group

--- Frontend/src/app/groups/join-to-group/join-to-group.component.ts
+++ Frontend/src/app/groups/join-to-group/join-to-group.component.ts
@@ -12,28 +12,32 @@ export class JoinToGroupComponent implements OnInit, OnDestroy {
 
   public isCodeWrong = false;
   public reditectToGroup = false;
-  public reditectToGroupMessage = '';
+  public errorMessage = '';
   private joinSubscribtion: Subscription;
 
   constructor(private groupsService: GroupsService, private router: Router) { }
 
-  public joinToGroup(formValues: JoinToGroupForm) {
+  public joinToGroup(formValues: JoinToGroupForm): void {
     this.joinSubscribtion = this.groupsService.joinToGroup(formValues).subscribe(
       (data) => {
         if (data.status === 200) {
           this.isCodeWrong = false;
-          this.reditectToGroupMessage = 'Dołączyłeś do grupy';
           this.reditectToGroup = true;
-          setTimeout(() => this.router.navigate(['groups', formValues.name]), 3000);
+          if (data.body) {
+            const groupID = JSON.parse(data.body).id;
+            this.isCodeWrong = false;
+            this.reditectToGroup = true;
+            setTimeout(() => this.router.navigate(['groups', groupID]), 3000);
+          }
         }
       },
       (error) => {
         if (error.status === 409) {
-          this.isCodeWrong = false;
-          this.reditectToGroupMessage = 'Już jesteś w tej grupie';
-          this.reditectToGroup = true;
-          setTimeout(() => this.router.navigate(['groups', formValues.name]), 3000);
+          this.errorMessage = 'Już jesteś w tej grupie.';
+          this.isCodeWrong = true;
+          this.reditectToGroup = false;
         } else {
+          this.errorMessage = 'Nieprawidłowy kod.';
           this.isCodeWrong = true;
           this.reditectToGroup = false;
         }
