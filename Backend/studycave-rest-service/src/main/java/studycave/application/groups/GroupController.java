dbea6ab1c9ed16b0bc0ff053c3563bcc32c85145
Drizzet <dawid.bogacki@gmail.com>
2018-12-10 13:20:46 +0100

    requested fix

--- Backend/studycave-rest-service/src/main/java/studycave/application/groups/GroupController.java
+++ Backend/studycave-rest-service/src/main/java/studycave/application/groups/GroupController.java
@@ -105,7 +105,7 @@ public class GroupController {
 	
 	@PostMapping("/{groupId}/tests")
 	public ResponseEntity<?> addTests(@PathVariable(required = true) String groupId, @RequestBody List<AddTestDto> testIds) {
-		return new ResponseEntity<>("Dodano", HttpStatus.OK);
+		return this.groupService.addTests(groupId, testIds);
 	}
 
 }
