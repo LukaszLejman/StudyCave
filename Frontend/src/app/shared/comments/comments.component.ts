3d0761478b0fce8e462506d000e5f3397559b4f6
Kublen <jakub.lenartowicz@gmail.com>
2018-12-17 11:43:20 +0100

    refresh comments

--- Frontend/src/app/shared/comments/comments.component.ts
+++ Frontend/src/app/shared/comments/comments.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy, OnChanges, SimpleChanges } from '@angular/core';
 import { SharedService } from '../shared.service';
 import { Subscription } from 'rxjs/Subscription';
 import { ActivatedRoute } from '@angular/router';
@@ -8,7 +8,7 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.css']
 })
-export class CommentsComponent implements OnInit, OnDestroy {
+export class CommentsComponent implements OnInit, OnChanges, OnDestroy {
 
   id: number;
   currentUser;
@@ -28,6 +28,13 @@ export class CommentsComponent implements OnInit, OnDestroy {
     this.id = this.route.snapshot.params.id;
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     this.commentsSubscription = this.sharedService.getComments(this.id, this.what).subscribe(data => this.comments = data);
+ }
+
+ ngOnChanges(changes: SimpleChanges) {
+   if (changes.comments.currentValue !== changes.comments.previousValue) {
+    this.comments = changes.comments.currentValue;
+   }
+
  }
 
   toggle() {
