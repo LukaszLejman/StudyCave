754a1e4916d2bbfe52c84ad8f7fbf34515da816b
Kublen <jakub.lenartowicz@gmail.com>
2018-12-29 18:53:35 +0100

    memory test tests fix

--- Frontend/src/app/flashcards/flashcards-memory-test/flashcards-memory-test.component.spec.ts
+++ Frontend/src/app/flashcards/flashcards-memory-test/flashcards-memory-test.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FlashcardsMemoryTestComponent } from './flashcards-memory-test.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { FlashcardsService } from '../flashcards.service';
+import { AuthenticationService } from '../../authentication.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('FlashcardsMemoryTestComponent', () => {
   let component: FlashcardsMemoryTestComponent;
@@ -8,7 +15,10 @@ describe('FlashcardsMemoryTestComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FlashcardsMemoryTestComponent ]
+      declarations: [ FlashcardsMemoryTestComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [FlashcardsService, AuthenticationService],
+      imports: [ RouterTestingModule, FormsModule, HttpClientModule, MatSnackBarModule]
     })
     .compileComponents();
   }));
