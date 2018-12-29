89c695c0852b606c5201c4c78281d7f2eaa66347
Kublen <jakub.lenartowicz@gmail.com>
2018-12-29 17:35:13 +0100

    authguard tests fix

--- Frontend/src/app/auth-guard.service.spec.ts
+++ Frontend/src/app/auth-guard.service.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { AuthGuard } from './auth-guard.service';
 
-describe('AuthGuardService', () => {
+fdescribe('AuthGuardService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthGuard]
+      providers: [AuthGuard],
+      imports: [RouterTestingModule]
     });
   });
 
