572271af021e1b9b72e8f5687f9ef58c6d9c62a9
Kublen <jakub.lenartowicz@gmail.com>
2018-12-29 18:01:11 +0100

    fcards filling tests fix

--- Frontend/src/app/flashcards/flashcards-filling-in-test/flashcards-filling-in-test.component.spec.ts
+++ Frontend/src/app/flashcards/flashcards-filling-in-test/flashcards-filling-in-test.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FlashcardsFillingInTestComponent } from './flashcards-filling-in-test.component';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { FlashcardsService } from '../flashcards.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthenticationService } from '../../authentication.service';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('FlashcardsFillingInTestComponent', () => {
   let component: FlashcardsFillingInTestComponent;
@@ -8,7 +15,10 @@ describe('FlashcardsFillingInTestComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FlashcardsFillingInTestComponent ]
+      declarations: [ FlashcardsFillingInTestComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [FlashcardsService, AuthenticationService],
+      imports: [ RouterTestingModule, FormsModule, HttpClientModule, MatSnackBarModule]
     })
     .compileComponents();
   }));
