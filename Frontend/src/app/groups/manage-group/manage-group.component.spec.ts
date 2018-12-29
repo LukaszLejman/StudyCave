f0f300ab64415f74c161a41991e891bfe57b2b20
Kublen <jakub.lenartowicz@gmail.com>
2018-12-29 19:13:22 +0100

    groups tests fix

--- Frontend/src/app/groups/manage-group/manage-group.component.spec.ts
+++ Frontend/src/app/groups/manage-group/manage-group.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ManageGroupComponent } from './manage-group.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { GroupsService } from '../groups.service';
+import { AuthenticationService } from '../../authentication.service';
+import { ConfirmationService } from 'primeng/api';
 
 describe('ManageGroupComponent', () => {
   let component: ManageGroupComponent;
@@ -8,7 +15,10 @@ describe('ManageGroupComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ManageGroupComponent ]
+      declarations: [ ManageGroupComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [ RouterTestingModule, HttpClientModule, MatSnackBarModule],
+      providers: [GroupsService, AuthenticationService, ConfirmationService]
     })
     .compileComponents();
   }));
