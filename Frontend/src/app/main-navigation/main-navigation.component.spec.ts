897fb88a604b43411f083c00028c081c41aed5b3
Kublen <jakub.lenartowicz@gmail.com>
2018-12-29 19:15:18 +0100

    ui tests fix

--- Frontend/src/app/main-navigation/main-navigation.component.spec.ts
+++ Frontend/src/app/main-navigation/main-navigation.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainNavigationComponent } from './main-navigation.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+import { AuthenticationService } from '../authentication.service';
 
 describe('MainNavigationComponent', () => {
   let component: MainNavigationComponent;
@@ -8,7 +14,10 @@ describe('MainNavigationComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainNavigationComponent ]
+      declarations: [ MainNavigationComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [ RouterTestingModule, HttpClientModule, MatSnackBarModule, FormsModule],
+      providers: [AuthenticationService]
     })
     .compileComponents();
   }));
