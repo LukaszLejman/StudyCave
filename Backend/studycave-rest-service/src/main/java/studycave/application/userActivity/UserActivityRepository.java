666d56336df85330669c732b0554df38b36ace10
Andrzej Fudro <andrzej.fudro@gmail.com>
2019-01-05 17:10:23 +0100

    endDate fix

--- Backend/studycave-rest-service/src/main/java/studycave/application/userActivity/UserActivityRepository.java
+++ Backend/studycave-rest-service/src/main/java/studycave/application/userActivity/UserActivityRepository.java
@@ -11,6 +11,6 @@ public interface UserActivityRepository extends JpaRepository<UserActivity, Long
     @Query("SELECT u FROM UserActivity u WHERE (u.fromUser.id = ?1 OR u.toUser.id = ?1) "
     		+ "AND (?2 is null OR u.group.id = ?2) AND (?3 is null OR u.date >= ?3) AND (?4 is null OR u.date <= ?4)")
     List<UserActivity> findAllByToUserOrFromUserAndGroupAndTime(Long userId, Long groupId,
-    		Date start, Date end);
+    		Date start, Timestamp end);
 
 }
